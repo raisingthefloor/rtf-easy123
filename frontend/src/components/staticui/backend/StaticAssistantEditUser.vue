e7dce7b77984729cad124e7e9810cfab625a550e
Jatin <jatin.raikwar@plenartech.com>
2021-08-10 21:49:23 +0530

    made changes to address book & sidebar added

--- frontend/src/components/staticui/backend/StaticAssistantEditUser.vue
+++ frontend/src/components/staticui/backend/StaticAssistantEditUser.vue
@@ -23,98 +23,164 @@ agreement nos. 289016 (Cloud4all) and 610510 (Prosperity4All)
 * Consumer Electronics Association Foundation
 -->
 <template>
-  <div class="container-fluid">
-    <nav class="mt-3" style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item"> <router-link to="/static/assistant/users">Users</router-link></li>
-        <li class="breadcrumb-item active" aria-current="page">Edit End User</li>
-      </ol>
-    </nav>
+  <main class="container-fluid pb-3 flex-grow-1 d-flex flex-column flex-sm-row overflow-auto">
+    <div class="row flex-grow-1 flex-grow-0 mt-2">
+      <div class="col-sm-2 flex-grow-sm-1 flex-shrink-1 flex-grow-0 sticky-top pb-sm-0 pb-3">
+        <div class="bg-light border rounded-3 p-1 h-100 sticky-top">
 
-
-
-    <div class="row">
-      <div class="col-md-1"></div>
-      <div class="col-md-10">
-        <h4 class="mb-3">Edit End User</h4>
-        <form class="">
-
-        </form>
-
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
-          <li class="nav-item" role="presentation">
-            <button class="nav-link active" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="true">Profile</button>
-          </li>
-          <li class="nav-item" role="presentation">
-            <button class="nav-link" id="imapdetails-tab" data-bs-toggle="tab" data-bs-target="#imapdetails" type="button" role="tab" aria-controls="imapdetails" aria-selected="false">IMAP details</button>
-          </li>
-          <li class="nav-item" role="presentation">
-            <button class="nav-link" id="addressbook-tab" data-bs-toggle="tab" data-bs-target="#addressbook" type="button" role="tab" aria-controls="addressbook" aria-selected="false">Address book</button>
-          </li>
-          <li class="nav-item" role="presentation">
-            <button class="nav-link" id="photos-tab" data-bs-toggle="tab" data-bs-target="#photos" type="button" role="tab" aria-controls="photos" aria-selected="false">Photos</button>
-          </li>
-          <li class="nav-item" role="presentation">
-            <button class="nav-link" id="easyweb-tab" data-bs-toggle="tab" data-bs-target="#easyweb" type="button" role="tab" aria-controls="easyweb" aria-selected="false">EasyWeb</button>
-          </li>
-        </ul>
-        <div class="tab-content" id="myTabContent">
-          <div class="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-            <div class="mb-3 mt-3">
-              <label for="firstname" class="form-label">First Name</label>
-              <input type="text" class="form-control" id="firstname">
-            </div>
-            <div class="mb-3">
-              <label for="lastname" class="form-label">Last Name</label>
-              <input type="text" class="form-control" id="lastname">
-            </div>
-            <div class="mb-3">
-              <label for="username" class="form-label">Username</label>
-              <input type="text" class="form-control" id="username">
-            </div>
-            <div class="mb-3">
-              <label for="password" class="form-label">Password</label>
-              <input type="password" class="form-control" id="password">
-            </div>
-            <button type="submit" class="btn btn-primary mb-5">Submit</button>
-          </div>
-          <div class="tab-pane fade" id="imapdetails" role="tabpanel" aria-labelledby="imapdetails-tab">
-            <div class="mb-3 mt-3">
-              <label for="host" class="form-label">Host</label>
-              <input type="text" class="form-control" id="host">
-            </div>
-            <div class="mb-3">
-              <label for="portnumber" class="form-label">Port Number</label>
-              <input type="text" class="form-control" id="portnumber">
-            </div>
-            <div class="mb-3">
-              <label for="imap_email" class="form-label">IMAP Email</label>
-              <input type="text" class="form-control" id="imap_email">
-            </div>
-            <div class="mb-3">
-              <label for="imap_password" class="form-label">IMAP Password</label>
-              <input type="text" class="form-control" id="imap_password">
-            </div>
-
-            <button type="submit" class="btn btn-primary mb-5">Submit</button>
-          </div>
-          <div class="tab-pane fade" id="addressbook" role="tabpanel" aria-labelledby="addressbook-tab">
-            <AddressBook class="mt-3"></AddressBook>
-          </div>
-          <div class="tab-pane fade" id="photos" role="tabpanel" aria-labelledby="photos-tab">Photos</div>
-          <div class="tab-pane fade" id="easyweb" role="tabpanel" aria-labelledby="easyweb-tab">EasyWeb</div>
+          <ul class="nav nav-pills flex-sm-column flex-row mb-auto justify-content-between text-truncate">
+            <li class="nav-item">
+              <a href="#" class="nav-link px-2 text-truncate">
+                <i class="bi bi-house fs-4"></i>
+                <span class="d-none d-sm-inline">Profile</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link px-2 text-truncate">
+                <i class="bi bi-speedometer fs-4"></i>
+                <span class="d-none d-sm-inline">IMAP Details</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link px-2 text-truncate"><i class="bi bi-card-text fs-4"></i>
+                <span class="d-none d-sm-inline">Address Book</span> </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link px-2 text-truncate"><i class="bi bi-bricks fs-4"></i>
+                <span class="d-none d-sm-inline">Photos</span> </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link px-2 text-truncate"><i class="bi bi-people fs-4"></i>
+                <span class="d-none d-sm-inline">EasyWeb</span> </a>
+            </li>
+          </ul>
         </div>
+      </div>
+      <div class="col overflow-auto h-100">
 
+        <nav class="mt-3" style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"> <router-link to="/static/assistant/users">Users</router-link></li>
+          <li class="breadcrumb-item active" aria-current="page">Edit End User</li>
+        </ol>
+      </nav>
+
+
+
+        <div class="row">
+        <div class="col-md-12">
+          <h4 class="mb-3">Edit End User</h4>
+
+          <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item" role="presentation">
+              <button class="nav-link active" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="true">Profile</button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button class="nav-link" id="imapdetails-tab" data-bs-toggle="tab" data-bs-target="#imapdetails" type="button" role="tab" aria-controls="imapdetails" aria-selected="false">IMAP details</button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button class="nav-link" id="addressbook-tab" data-bs-toggle="tab" data-bs-target="#addressbook" type="button" role="tab" aria-controls="addressbook" aria-selected="false">Address book</button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button class="nav-link" id="photos-tab" data-bs-toggle="tab" data-bs-target="#photos" type="button" role="tab" aria-controls="photos" aria-selected="false">Photos</button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button class="nav-link" id="easyweb-tab" data-bs-toggle="tab" data-bs-target="#easyweb" type="button" role="tab" aria-controls="easyweb" aria-selected="false">EasyWeb</button>
+            </li>
+          </ul>
+          <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+              <div class="mb-3 mt-3">
+                <label for="firstname" class="form-label">First Name</label>
+                <input type="text" class="form-control" id="firstname">
+              </div>
+              <div class="mb-3">
+                <label for="lastname" class="form-label">Last Name</label>
+                <input type="text" class="form-control" id="lastname">
+              </div>
+              <div class="mb-3">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" class="form-control" id="username">
+              </div>
+              <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password">
+              </div>
+              <button type="submit" class="btn btn-primary mb-5">Submit</button>
+            </div>
+            <div class="tab-pane fade" id="imapdetails" role="tabpanel" aria-labelledby="imapdetails-tab">
+              <div class="mb-3 mt-3">
+                <label for="host" class="form-label">Host</label>
+                <input type="text" class="form-control" id="host">
+              </div>
+              <div class="mb-3">
+                <label for="portnumber" class="form-label">Port Number</label>
+                <input type="text" class="form-control" id="portnumber">
+              </div>
+              <div class="mb-3">
+                <label for="imap_email" class="form-label">IMAP Email</label>
+                <input type="text" class="form-control" id="imap_email">
+              </div>
+              <div class="mb-3">
+                <label for="imap_password" class="form-label">IMAP Password</label>
+                <input type="text" class="form-control" id="imap_password">
+              </div>
+
+              <button type="submit" class="btn btn-primary mb-5">Submit</button>
+            </div>
+            <div class="tab-pane fade" id="addressbook" role="tabpanel" aria-labelledby="addressbook-tab">
+              <AddressBook class="mt-3"></AddressBook>
+            </div>
+            <div class="tab-pane fade" id="photos" role="tabpanel" aria-labelledby="photos-tab">Photos</div>
+            <div class="tab-pane fade" id="easyweb" role="tabpanel" aria-labelledby="easyweb-tab">EasyWeb</div>
+          </div>
+
+        </div>
+      </div>
       </div>
     </div>
+  </main>
 
-
-  </div>
 </template>
 
 <style scoped src="@/assets/bootstrap-5/css/bootstrap.min.css">
 </style>
+<style scoped>
+/*
+ * Off Canvas
+ * --------------------------------------------------
+ */
+@media screen and (max-width: 768px) {
+  .row-offcanvas {
+    position: relative;
+    -webkit-transition: all 0.25s ease-out;
+    -moz-transition: all 0.25s ease-out;
+    transition: all 0.25s ease-out;
+    background:#ecf0f1;
+  }
 
+  .row-offcanvas-left
+  .sidebar-offcanvas {
+    left: -40%;
+  }
+
+  .row-offcanvas-left.active {
+    left: 40%;
+  }
+
+  .sidebar-offcanvas {
+    position: absolute;
+    top: 0;
+    width: 40%;
+    margin-left: 12px;
+  }
+}
+
+#sidebar {
+  padding:15px;
+  margin-top:10px;
+}
+</style>
 
 <script>
 import AddressBook from './components/AddressBook'
--- frontend/src/components/staticui/backend/components/AddressBook.vue
+++ frontend/src/components/staticui/backend/components/AddressBook.vue
@@ -1,14 +1,50 @@
 <template>
   <div class="row">
+    <div class="col-md-12">
+      <a href="javascript:void(0)" class="btn btn-sm btn-primary float-end mb-3" @click="addContact">Add Contact</a>
+    </div>
     <div class="col-md-3">
       <div class="address-book" style="position: relative;">
-        <div class="list-group" style="max-height: 75vh; overflow-y: scroll;">
-          <a href="#" class="list-group-item list-group-item-action" v-bind:class="(current_contact.id == contact.id)?'active':''"  v-for="contact in contacts" :key="contact.id" @click="showContact(contact.id)">
+        <input type="text" style="width: 100%; height: 35px;" v-model="search_contact" placeholder="Search All Contacts">
+        <div class="list-group address-book-contact-list" id="address-book-contact-list" style="max-height: 75vh; overflow-y: scroll;">
+          <a href="#" class="list-group-item list-group-item-action address-book-list-item" v-bind:class="(current_contact.id == contact.id)?'active':''"  v-for="contact in contacts" :key="contact.id" @click="showContact(contact.id)">
             {{ contact.first_name + " " + contact.last_name }}
           </a>
 
         </div>
-        <div class="slider-nav" style="position: absolute; max-height: 70vh; top: 20px; right: 0px;"><ul><li><a alt="#a">a</a></li><li><a alt="#b">b</a></li><li><a alt="#c">c</a></li><li><a alt="#d">d</a></li><li><a alt="#e">e</a></li><li><a alt="#f">f</a></li><li><a alt="#g">g</a></li><li><a alt="#h">h</a></li><li><a alt="#i">i</a></li><li><a alt="#j">j</a></li><li><a alt="#k">k</a></li><li><a alt="#l">l</a></li><li><a alt="#m">m</a></li><li><a alt="#n">n</a></li><li><a alt="#o">o</a></li><li><a alt="#p">p</a></li><li><a alt="#q">q</a></li><li><a alt="#r">r</a></li><li><a alt="#s">s</a></li><li><a alt="#t">t</a></li><li><a alt="#u">u</a></li><li><a alt="#v">v</a></li><li><a alt="#w">w</a></li><li><a alt="#x">x</a></li><li><a alt="#y">y</a></li><li><a alt="#z">z</a></li></ul></div>
+
+        <div class="slider-nav" style="position: absolute; max-height: 70vh; top: 0px; right: 0px;">
+          <ul>
+            <li><a href="javascript:void(0)" @click="moveToContact('a')" alt="#a">a</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('b')" alt="#b">b</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('c')" alt="#c">c</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('d')" alt="#d">d</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('e')" alt="#e">e</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('f')" alt="#f">f</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('g')" alt="#g">g</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('h')" alt="#h">h</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('i')" alt="#i">i</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('j')" alt="#j">j</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('k')" alt="#k">k</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('l')" alt="#l">l</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('m')" alt="#m">m</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('n')" alt="#n">n</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('o')" alt="#o">o</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('p')" alt="#p">p</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('q')" alt="#q">q</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('r')" alt="#r">r</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('s')" alt="#s">s</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('t')" alt="#t">t</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('u')" alt="#u">u</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('v')" alt="#v">v</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('w')" alt="#w">w</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('x')" alt="#x">x</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('y')" alt="#y">y</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('z')" alt="#z">z</a></li>
+            <li><a href="javascript:void(0)" @click="moveToContact('#')" alt="#z">#</a></li>
+          </ul>
+
+        </div>
       </div>
 
     </div>
@@ -16,15 +52,42 @@
       <div class="card">
         <div class="card-body">
           <div class="row g-0">
-            <div class="col-md-4">
-<!--              <img src="https://picsum.photos/200/250" class="img-fluid rounded-start" alt="...">-->
-              <img :src="current_contact.image" class="img-fluid rounded-start" alt="...">
+            <div v-show="show_add_contact_form">
+              <div class="mb-3 mt-3">
+                <label for="firstname" class="form-label">First Name</label>
+                <input type="text" class="form-control" id="firstname" v-model="first_name">
+              </div>
+              <div class="mb-3">
+                <label for="lastname" class="form-label">Last Name</label>
+                <input type="text" class="form-control" id="lastname" v-model="last_name">
+              </div>
+              <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" v-model="email">
+              </div>
+              <file-pond
+                  name="test"
+                  ref="pond"
+                  label-idle="Drop files here..."
+                  v-bind:allow-multiple="true"
+                  accepted-file-types="image/jpeg, image/png"
+                  server="/api"
+                  v-bind:files="myFiles"
+                  v-on:init="handleFilePondInit"
+              />
+              <button class="btn btn-primary" @click="saveContact">Add</button>
             </div>
-            <div class="col-md-8">
+            <div class="col-md-4" v-show="!show_add_contact_form">
+<!--              <img src="https://picsum.photos/200/250" class="img-fluid rounded-start" alt="...">-->
+              <img :src="current_contact.image" class="img-fluid rounded-start" alt="..." width="200" height="250">
+            </div>
+            <div class="col-md-8" v-show="!show_add_contact_form">
+
               <div class="card-body">
                 <h5 class="card-title">{{ current_contact.first_name }} {{ current_contact.last_name }}</h5>
                 <p class="card-text">{{ current_contact.email }}</p>
                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <a href="javascript:void(0)" class="btn btn-sm btn-danger" @click="deleteContatct(current_contact.id)">Delete Contact</a>
               </div>
             </div>
           </div>
@@ -37,14 +100,105 @@
 <style scoped src="@/assets/bootstrap-5/css/bootstrap.min.css">
 </style>
 
+<style scoped>
+.slider-nav {
+  z-index: 5;
+}
+.slider-nav>ul {
+  list-style: none;
+  background-color: #efeeee;
+  padding: 0px;
+  text-decoration: none;
+  text-transform: capitalize;
+  font-size: 0.8rem;
+}
+.slider-nav>ul>li {
+  margin-bottom: 0.2rem;
+}
+.slider-nav>ul>li>a {
+  text-decoration: none;
+}
+/* width */
+.address-book-contact-list::-webkit-scrollbar {
+  width: 3px;
+}
+
+/* Track */
+.address-book-contact-list::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+.address-book-contact-list::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+/* Handle on hover */
+.address-book-contact-list::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+</style>
+
 <script>
+// Import Vue FilePond
+import vueFilePond from "vue-filepond";
+
+// Import FilePond styles
+import "filepond/dist/filepond.min.css";
+
+// Import image preview plugin styles
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css";
+// Import the plugin code
+import FilePondPluginImageCrop from 'filepond-plugin-image-crop';
+
+// Import image preview and file type validation plugins
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+
+// Create component
+const FilePond = vueFilePond(
+    FilePondPluginFileValidateType,
+    FilePondPluginImagePreview
+);
+
+FilePond.registerPlugin(FilePondPluginImageCrop)
+
+
 import contacts from './fackcontacts.json'
 export default {
   name: 'AddressBook',
+  components: {
+    FilePond,
+  },
   data() {
     return {
       contacts: contacts,
-      current_contact: {}
+      current_contact: {},
+      search_contact: null,
+      show_add_contact_form: false,
+      first_name: null,
+      last_name: null,
+      email: null,
+
+      id: 2000,
+      myFiles: []
+    }
+  },
+  watch: {
+    search_contact(newVal) {
+      let filter = newVal.toUpperCase()
+      let li = document.getElementsByClassName("address-book-list-item")
+
+      for (let i = 0; i < li.length; i++) {
+
+        let txtValue = li[i].textContent || li[i].innerText
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+        } else {
+          li[i].style.display = "none";
+        }
+      }
+
     }
   },
   mounted() {
@@ -58,7 +212,78 @@ export default {
     showContact(id) {
       this.current_contact = this.contacts.find(obj => obj.id == id)
       //console.log(this.current_contact)
-    }
+    },
+    moveToContact(name) {
+      //console.log(name)
+      //check if char
+      if((/[a-zA-Z]/).test(name))
+      {
+        //char
+      }
+      else {
+        //not a char
+      }
+
+
+      // get the "Div" inside which you wish to scroll (i.e. the container element)
+      const El = document.getElementById('address-book-contact-list');
+
+
+      //get the height of each element
+      //console.log("offsetHeight", El.firstChild.offsetHeight, El.firstChild.clientHeight)
+
+      //get the element matching index
+      let move_to_index = 0
+      for (let i = 0; i < this.contacts.length; i++)
+      {
+        if(this.contacts[i].first_name.charAt(0) == name || this.contacts[i].first_name.charAt(0) == name.toUpperCase())
+        {
+          move_to_index = i
+          if(move_to_index > 1)
+          {
+            move_to_index = move_to_index-1
+          }
+          //console.log("move_to_index", move_to_index, this.contacts[i].first_name)
+          // Lets say you wish to scroll by 100px,
+          El.scrollTo({top: (El.firstChild.offsetHeight * move_to_index), behavior: 'smooth'});
+          break;
+        }
+      }
+
+
+
+
+
+
+
+      // If you wish to scroll until the end of the container
+      //El.scrollTo({top: El.scrollHeight, behavior: 'smooth'});
+    },
+    deleteContatct(id) {
+      this.contacts = this.contacts.filter(obj => obj.id != id)
+      this.current_contact = this.contacts[0]
+    },
+    addContact() {
+      this.show_add_contact_form = true
+    },
+    saveContact() {
+      this.contacts.push({
+        id: this.id,
+        first_name: this.first_name,
+        last_name: this.last_name,
+        email: this.email
+      })
+      this.contacts.sort(function (a, b) {
+        return a.first_name.localeCompare(b.first_name)
+      })
+      this.show_add_contact_form = false
+      this.id = this.id + 1
+    },
+    handleFilePondInit: function () {
+      console.log("FilePond has initialized");
+
+      // FilePond instance methods are available on `this.$refs.pond`
+    },
   }
 }
 </script>
